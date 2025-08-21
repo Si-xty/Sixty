48e3032f754fbe8c272aea6a6ae6c4fd83c923d3
Cristian <kristianhernandez.2003@gmail.com>
2025-08-21 04:21:09 +0100

    Implementing 'Proximamente' view for sections not yet created

---
+++ application/controllers/ProximamenteController.php
@@ -0,0 +1,28 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @property CI_Session $session
+ * @property CI_Input $input
+**/
+
+class ProximamenteController extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        // if($this->session->has_userdata('authenticated'))
+        // {   
+        //     // echo json_encode(['status' => 'success', 'message' => 'Registrado correctamente']);
+        //     $this->session->set_flashdata('status', 'Ya estas logeado');
+        //     redirect(base_url('welcome'));
+        // }
+
+        $this->load->model('UserModel');
+    }
+
+    public function index()
+    {
+        $this->load->view('proximamente');
+    }
+}
---
+++ application/views/proximamente.php
@@ -0,0 +1,14 @@
+<div style="
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  height:100vh;
+  margin:0;
+  font-family: system-ui, Arial, sans-serif;
+  font-weight: 500;
+  font-size: clamp(40px, 10vw, 120px);
+  letter-spacing: 2px;
+  text-align: center;
+">
+  Próximamente!...
+</div>
